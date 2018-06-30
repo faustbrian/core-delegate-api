9fea7f7be3d6d8f21a5d37f05e6b4ef6d09a8b61
Brian Faust <hello@brianfaust.me>
2018-06-30 12:28:37 +0300

    test: setup test boilerplate

---
+++ __tests__/server/transformers/voter.test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * This file is part of Ark Core - Delegate API.
+ *
+ * (c) ArkX <hello@arkx.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+describe('Transformer - Voter', () => {
+    it('should be ok', () => {
+        expect(true).toBeTruthy()
+    })
+})
